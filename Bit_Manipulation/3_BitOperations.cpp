aeb0d6e4d8a13425fea50265411f6cca7b7dac61
Nishtha <nishtha7038@gmail.com>
2025-08-11 23:42:37 +0530

    first commit

---
+++ Bit_Manipulation/3_BitOperations.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a=5;
+    int b=10;
+
+    cout<<"And Operation : "<<(a & b)<<endl;
+    cout<<"Or Operation : "<<(a | b)<<endl;
+    cout<<"Xor Operation : "<<(a ^ b)<<endl;
+    cout<<"Not Operation : "<<(~a)<<endl;
+
+}
